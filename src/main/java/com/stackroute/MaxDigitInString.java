ea484af06e5f454136c16bbacff232cb42f8de2d
ubuntu <ubuntu@ip-172-31-32-139.ec2.internal>
2020-01-22 16:06:24 +0000

    Refactored code to pass hobbes falied test cases

--- src/main/java/com/stackroute/MaxDigitInString.java
+++ src/main/java/com/stackroute/MaxDigitInString.java
@@ -8,20 +8,27 @@ import java.util.Scanner;
 public class MaxDigitInString {
 
 	public static int getMaxDigit(String alphanumeric) {
-		char [] chrArr = alphanumeric.toCharArray();
-		List<Integer> intList = new ArrayList<Integer>();
-		for (int i = 0; i < chrArr.length; i++) {
-			if(Character.isDigit(chrArr[i])) {
-				int num = chrArr[i] - '0';
-				intList.add(num);
+		if(alphanumeric != null) {
+			if(alphanumeric.matches("[^a-z0-9A-Z]")) {
+				return -1;
+			}else {
+				char [] chrArr = alphanumeric.toCharArray();
+				List<Integer> intList = new ArrayList<Integer>();
+				for (int i = 0; i < chrArr.length; i++) {
+					if(Character.isDigit(chrArr[i])) {
+						int num = chrArr[i] - '0';
+						intList.add(num);
+					}
+					
+				}
+				if(intList.size()>0) {
+					Collections.sort(intList);
+					return intList.get(intList.size()-1);
+				}
 			}
+			
 		}
-		if(intList.size()>0) {
-			Collections.sort(intList);
-			return intList.get(intList.size()-1);
-		}
-		
-		return 0;
+		return -1;
 	}
 
    	//Do not print anything other than what's expected/asked in problem
@@ -30,7 +37,7 @@ public class MaxDigitInString {
     	Scanner sc = new Scanner(System.in);
 		String regNumber = sc.nextLine();
 		int res = getMaxDigit(regNumber);
-		if(res>0) {
+		if(res>=0) {
 		System.out.println("Max digit:"+res);
 		}else {
 			System.out.println("No digits in string");
